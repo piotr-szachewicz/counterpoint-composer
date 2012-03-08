d004468073e621071ae7be7e4e46eeb4adcc8e1d
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2012-03-08 20:59:23 +0100

    Switched to Maven multiproject project.

---
+++ CounterpointComposer/src/main/java/pl/szachewicz/App.java
@@ -0,0 +1,14 @@
+package pl.szachewicz;
+
+import jm.JMC;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" + JMC.DYNAMIC);
+    }
+}
