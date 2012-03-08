64f296592e8609d777ba312fd7fce294689e876d
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2012-03-09 00:21:19 +0100

    Able to read/write/play music.

--- CounterpointComposer/src/main/java/pl/szachewicz/view/MainFrame.java
+++ CounterpointComposer/src/main/java/pl/szachewicz/view/MainFrame.java
@@ -1,38 +1,97 @@
 package pl.szachewicz.view;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.io.File;
 
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-
-import jm.gui.cpn.TrebleStave;
 
 public class MainFrame extends JFrame {
 
 	public static int WINDOW_HEIGHT = 350;
-	public static int WINDOW_WIDTH = 700;
-	
-	private StavePanel stavePanel;
+	public static int WINDOW_WIDTH = 900;
+
+	private final StavePanel stavePanel;
+
+	JFileChooser fileChooser = new JFileChooser();
 
 	public MainFrame() {
 		super();
 		this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		this.setTitle("Counterpoint composer");
 
+		createMenu();
+
 		JPanel panel = new JPanel(new BorderLayout());
 		panel.add(new JLabel("bebe"), BorderLayout.NORTH);
-		
+
 		stavePanel = new StavePanel();
 		panel.add(stavePanel, BorderLayout.CENTER);
-		
+
 		JPanel buttonsPanel = new JPanel();
-		JButton playButton = new JButton("Play");
+		JButton playButton = new JButton(new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				stavePanel.playScore();
+			}
+		});
+		playButton.setText("Play");
 		buttonsPanel.add(playButton);
-		
+
 		panel.add(buttonsPanel, BorderLayout.SOUTH);
 		this.add(panel);
+	}
+
+	protected void createMenu() {
+		JMenuBar menuBar = new JMenuBar();
+
+		JMenu menu = new JMenu("File");
+		JMenuItem saveMenuItem = new JMenuItem(new SaveAction());
+		menu.add(saveMenuItem);
+		JMenuItem loadMenuItem = new JMenuItem(new LoadJmAction());
+		menu.add(loadMenuItem);
+
+		menuBar.add(menu);
+
+		this.setJMenuBar(menuBar);
+	}
+
+	class SaveAction extends AbstractAction {
+
+		public SaveAction() {
+			super("Save");
+		}
+
+		public void actionPerformed(ActionEvent arg0) {
+			int returnVal = fileChooser.showSaveDialog(MainFrame.this);
+	        if (returnVal == JFileChooser.APPROVE_OPTION) {
+	        	File selectedFile = fileChooser.getSelectedFile();
+	        	stavePanel.saveResults(selectedFile.getAbsolutePath());
+	        }
+		}
+
+	}
+
+	class LoadJmAction extends AbstractAction {
+		public LoadJmAction() {
+			super("Load JM");
+		}
+
+		public void actionPerformed(ActionEvent arg0) {
+			int returnVal = fileChooser.showOpenDialog(MainFrame.this);
+	        if (returnVal == JFileChooser.APPROVE_OPTION) {
+	        	File selectedFile = fileChooser.getSelectedFile();
+	        	stavePanel.loadScoreFromJMFile(selectedFile.getAbsolutePath());
+	        }
+
+		}
 	}
 
 }
--- CounterpointComposer/src/main/java/pl/szachewicz/view/StavePanel.java
+++ CounterpointComposer/src/main/java/pl/szachewicz/view/StavePanel.java
@@ -4,35 +4,74 @@ import java.awt.Dimension;
 
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 
-import jm.constants.Pitches;
 import jm.gui.cpn.BassStave;
 import jm.gui.cpn.TrebleStave;
+import jm.music.data.Part;
+import jm.music.data.Phrase;
+import jm.music.data.Score;
+import jm.util.Play;
+import jm.util.Read;
+import jm.util.Write;
 
 public class StavePanel extends JPanel {
 
-	private TrebleStave trebleStave;
-	private BassStave bassStave;
-	
+	private final TrebleStave trebleStave;
+	private final BassStave bassStave;
+
 	public StavePanel() {
-		
+
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		
+
 		trebleStave = new TrebleStave();
 		bassStave = new BassStave();
-		
+
 		panel.add(trebleStave);
 		panel.add(bassStave);
-		
+
 		JScrollPane scrollPane = new JScrollPane(panel);
 		scrollPane.setPreferredSize(new Dimension(MainFrame.WINDOW_WIDTH, 250));
 		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		
+
 		this.add(scrollPane);
-		
+
+	}
+
+	protected Score getScore() {
+		Score score = new Score();
+		Part treblePart = new Part(trebleStave.getPhrase());
+		Part bassPart = new Part(bassStave.getPhrase());
+		score.add(treblePart);
+		score.add(bassPart);
+
+		return score;
+	}
+
+	public void saveResults(String filePath) {
+
+		Score score = getScore();
+		Write.jm(score, filePath);
+	}
+
+	public void loadScoreFromJMFile(String filePath) {
+		Score score = new Score();
+		Read.jm(score, filePath);
+
+		Part[] parts = score.getPartArray();
+
+		Phrase treblePhrase = parts[0].getPhrase(0);
+		trebleStave.setPhrase(treblePhrase);
+
+		if (parts.length > 1) {
+			Phrase bassPhrase = parts[1].getPhrase(0);
+			bassStave.setPhrase(bassPhrase);
+		}
+	}
+
+	public void playScore() {
+		Play.midi(getScore());
 	}
 }
