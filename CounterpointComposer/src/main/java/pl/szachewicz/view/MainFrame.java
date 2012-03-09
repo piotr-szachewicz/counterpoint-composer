ba508cfa368cb97db8cc51febb2763961cdbd2ad
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2012-03-09 10:32:40 +0100

    Added default close operation to frame.

--- CounterpointComposer/src/main/java/pl/szachewicz/view/MainFrame.java
+++ CounterpointComposer/src/main/java/pl/szachewicz/view/MainFrame.java
@@ -24,6 +24,7 @@ public class MainFrame extends JFrame {
 
 	public MainFrame() {
 		super();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		this.setTitle("Counterpoint composer");
 
